37a01e5df58c49efe84ffc636a33d35c1cb5e490
Niranjan <niranjansaikoppisetti@gmail.com>
2025-05-27 16:21:24 +0530

    Code enhancements

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hi')
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_text_length(text:str) -> int:
+    """Returns the length of a text by characters."""
+    return len(text)
+
+
+if __name__ == "__main__":
+    print("Hello React LangChain.")
+    print(get_text_length(text='dog'))
